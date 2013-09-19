d3f93978cac2fc35a633dc8f9e1fab245a169535
phoenix7782 <robert@sparkk.us>
2013-09-19 18:12:53 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+TigerLaundry::Application.config.secret_key_base = '0fab52592436edc521d4a4cd95a44898b924289889b8ce2765caa268e008cd7e7df8ed6563e4b540e6d16fb062bfa1e227ba569bfa918a718f11e16bd3b424d0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+TigerLaundry::Application.config.session_store :cookie_store, key: '_TigerLaundry_session'
