43c69108f17010981c60d5516e904b87448cfef2
Robert McLaughlin <robert@sparkk.us>
2013-10-12 23:05:34 -0400

    Users' names and emails now auto-load from LDAP

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,18 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise  :ldap_authenticatable, authentication_keys: [:username]
+  devise  :ldap_authenticatable, :rememberable, authentication_keys: [:username]
 
   has_many :submissions
+
+  before_save do
+    Rails.logger.debug "############"
+    Rails.logger.debug username
+    entry = Devise::LDAP::Adapter.get_ldap_entry(username)
+    Rails.logger.debug "!!!!!!!!!!!!!!!!!!!!!!!"
+    self.name  = entry[:cn][0]
+    self.email = username + "@rit.edu"
+  end
 
   def admin?
     privilege_level >= 100
