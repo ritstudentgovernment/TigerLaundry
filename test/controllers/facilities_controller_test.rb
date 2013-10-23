2311406286784df3facbe1892273d7d39e5f300b
Robert McLaughlin <robert@sparkk.us>
2013-10-22 20:26:48 -0400

    added devise helpers to facilities tests

--- test/controllers/facilities_controller_test.rb
+++ test/controllers/facilities_controller_test.rb
@@ -1,24 +1,34 @@
 require 'test_helper'
 
 class FacilitiesControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
+
   setup do
-    @facility = facilities(:one)
+    @facility     = facilities(:one)
+    @admin_user   = users(:admin)
+    @mod_user     = users(:mod)
+    @regular_user = users(:regular)
   end
 
   test "should get index" do
+    sign_in @regular_user
     get :index
     assert_response :success
     assert_not_nil assigns(:facilities)
   end
 
   test "should get new" do
+    sign_in @admin_user
     get :new
     assert_response :success
   end
 
   test "should create facility" do
+    sign_in @admin_user
     assert_difference('Facility.count') do
-      post :create, facility: { driers: @facility.driers, name: @facility.name, washers: @facility.washers }
+      post :create, facility: { driers: @facility.driers,
+                                name: @facility.name,
+                                washers: @facility.washers }
     end
 
     assert_redirected_to facility_path(assigns(:facility))
@@ -30,16 +40,21 @@ class FacilitiesControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
+    sign_in @admin_user
     get :edit, id: @facility
     assert_response :success
   end
 
   test "should update facility" do
-    patch :update, id: @facility, facility: { driers: @facility.driers, name: @facility.name, washers: @facility.washers }
+    sign_in @admin_user
+    patch :update, id: @facility, facility: { driers: @facility.driers,
+                                              name: @facility.name,
+                                              washers: @facility.washers }
     assert_redirected_to facility_path(assigns(:facility))
   end
 
   test "should destroy facility" do
+    sign_in @admin_user
     assert_difference('Facility.count', -1) do
       delete :destroy, id: @facility
     end
