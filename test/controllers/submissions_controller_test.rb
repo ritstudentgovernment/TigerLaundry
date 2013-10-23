095ff568d0e57ded190f611c7827694c12d1a8df
Robert McLaughlin <robert@sparkk.us>
2013-10-23 19:30:45 -0400

    updated submissions controller tests for new fixtures

--- test/controllers/submissions_controller_test.rb
+++ test/controllers/submissions_controller_test.rb
@@ -4,27 +4,23 @@ class SubmissionsControllerTest < ActionController::TestCase
   include Devise::TestHelpers
 
   setup do
-    @submission   = submissions(:one)
-    @admin_user   = users(:admin)
-    @mod_user     = users(:mod)
-    @regular_user = users(:regular)
+    @submission   = submissions(:anonymous_submission)
+    @user   = users(:admin)
+    sign_in @user
   end
 
   test "should get index" do
-    sign_in @regular_user
     get :index, facility_id: @submission.facility_id
     assert_response :success
     assert_not_nil assigns(:submissions)
   end
 
   test "should get new" do
-    sign_in @regular_user
     get :new, facility_id: @submission.facility_id
     assert_response :success
   end
 
   test "should create submission" do
-    sign_in @regular_user
     assert_difference('Submission.count') do
       post :create,
            facility_id: @submission.facility_id,
@@ -38,19 +34,16 @@ class SubmissionsControllerTest < ActionController::TestCase
   end
 
   test "should show submission" do
-    sign_in @regular_user
     get :show, id: @submission, facility_id: @submission.facility_id
     assert_response :success
   end
 
   test "should get edit" do
-    sign_in @admin_user
     get :edit, id: @submission, facility_id: @submission.facility_id
     assert_response :success
   end
 
   test "should update submission" do
-    sign_in @admin_user
     patch :update,
           id: @submission,
           facility_id: @submission.facility_id,
@@ -62,7 +55,6 @@ class SubmissionsControllerTest < ActionController::TestCase
   end
 
   test "should destroy submission" do
-    sign_in @admin_user
     assert_difference('Submission.count', -1) do
       delete :destroy, id: @submission, facility_id: @submission.facility_id
     end
@@ -71,7 +63,6 @@ class SubmissionsControllerTest < ActionController::TestCase
   end
 
   test "should get limited" do
-    sign_in @regular_user
     get :limited, facility_id: @submission.facility_id, format: :json
     assert_response :success
   end
