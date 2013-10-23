54bd856ef296a1cdfd535709e310c3268bcff1f9
Robert McLaughlin <robert@sparkk.us>
2013-10-23 19:30:20 -0400

    updated facilities controller tests for new fixtures

--- test/controllers/facilities_controller_test.rb
+++ test/controllers/facilities_controller_test.rb
@@ -4,27 +4,23 @@ class FacilitiesControllerTest < ActionController::TestCase
   include Devise::TestHelpers
 
   setup do
-    @facility     = facilities(:one)
-    @admin_user   = users(:admin)
-    @mod_user     = users(:mod)
-    @regular_user = users(:regular)
+    @facility     = facilities(:ellingson)
+    @user   = users(:admin)
+    sign_in @user
   end
 
   test "should get index" do
-    sign_in @regular_user
     get :index
     assert_response :success
     assert_not_nil assigns(:facilities)
   end
 
   test "should get new" do
-    sign_in @admin_user
     get :new
     assert_response :success
   end
 
   test "should create facility" do
-    sign_in @admin_user
     assert_difference('Facility.count') do
       post :create, facility: { driers: @facility.driers,
                                 name: @facility.name,
@@ -40,13 +36,11 @@ class FacilitiesControllerTest < ActionController::TestCase
   end
 
   test "should get edit" do
-    sign_in @admin_user
     get :edit, id: @facility
     assert_response :success
   end
 
   test "should update facility" do
-    sign_in @admin_user
     patch :update, id: @facility, facility: { driers: @facility.driers,
                                               name: @facility.name,
                                               washers: @facility.washers }
@@ -54,7 +48,6 @@ class FacilitiesControllerTest < ActionController::TestCase
   end
 
   test "should destroy facility" do
-    sign_in @admin_user
     assert_difference('Facility.count', -1) do
       delete :destroy, id: @facility
     end
