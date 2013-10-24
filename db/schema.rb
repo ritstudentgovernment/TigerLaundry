b4de247de42d08626cbd34d9a63e734d7681ce64
Robert McLaughlin <robert@sparkk.us>
2013-10-23 21:41:17 -0400

    fixed remembe me button giving an error on login

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131012190029) do
+ActiveRecord::Schema.define(version: 20131024013929) do
 
   create_table "facilities", force: true do |t|
     t.integer  "washers"
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20131012190029) do
     t.string   "name"
     t.integer  "privilege_level",        default: 0,  null: false
     t.string   "username"
+    t.string   "remember_token"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
